1309a634ffa507cf2e0c2c1b23e5ff882bd951b3
Manca Florimond <florimond.manca@student.ecp.fr>
2017-05-20 22:22:06 +0200

    Simplify further random shoot controller

--- src/components/random_shoot_controller.lua
+++ src/components/random_shoot_controller.lua
@@ -7,26 +7,28 @@ function Component:initialize(arguments)
     arguments.meanWaitTime = arguments.meanWaitTime or 1
     arguments.waitTimeSigma = arguments.waitTimeSigma or 1
     rope.Component.initialize(self, arguments)
-    self.time = 0
-    self.waitTime = self:makeWaitTime()
 end
 
 function Component:awake()
     self.shooter = self.gameObject:getComponent('components.shooter')
+    self:resetTimer()
 end
 
-function Component:makeWaitTime()
+function Component:resetTimer()
+    self.time = 0
     -- normally distributed (0 mean, 1 standard deviation) random number
     local randn = math.sqrt(-2*math.log(love.math.random())) * math.cos(2*math.pi*love.math.random())
-    return self.meanWaitTime + self.waitTimeSigma * randn
+    self.waitTime = self.meanWaitTime + self.waitTimeSigma * randn
 end
 
+function Component:tick(dt) self.time = self.time + dt end
+function Component:timeToShoot() return self.time > self.waitTime end
+
 function Component:update(dt)
-    self.time = self.time + dt
-    if self.time > self.waitTime then
+    self:tick(dt)
+    if self:timeToShoot() then
         self.shooter:shoot()
-        self.time = 0
-        self.waitTime = self:makeWaitTime()
+        self:resetTimer()
     end
 end
 
