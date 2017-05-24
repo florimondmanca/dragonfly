baa43d8d7445eccc22cc58c24f3d50220bf64284
Manca Florimond <florimond.manca@student.ecp.fr>
2017-05-24 23:07:02 +0200

    Check collision edge-edge, edge-rectangle

--- src/scenes/colliders_test.lua
+++ src/scenes/colliders_test.lua
@@ -14,38 +14,49 @@ return {
                 },
             },
         },
-        -- {
-        --     name = 'Edge',
-        --     transform = {position = {x = 400, y = 300}},
-        --     components = {
-        --         {
-        --             script = 'rope.builtins.colliders.edge_collider',
-        --             arguments = {point1 = {x=0, y=-100}, point2={x=0, y=100}},
-        --         },
-        --     }
-        -- },
         {
-            name = 'Rectangle',
+            name = 'Edge',
             transform = {position = {x = 400, y = 300}},
             components = {
                 {
-                    script = 'rope.builtins.colliders.rectangle_collider',
-                    arguments = {
-                        width = 300,
-                        height = 200
-                    },
+                    script = 'rope.builtins.colliders.edge_collider',
+                    arguments = {point1 = {x=0, y=-100}, point2={x=0, y=100}},
                 },
             }
         },
         {
-            name = 'Circle',
+            name = 'Edge',
+            transform = {position = {x = 400, y = 300}},
             components = {
                 {
-                    script = 'rope.builtins.colliders.circle_collider',
-                    arguments = {radius = 100},
+                    script = 'rope.builtins.colliders.edge_collider',
+                    arguments = {point1 = {x=200, y=-100}, point2={x=0, y=100}},
                 },
                 {script = 'rope.builtins.mouse.link'}
             }
         },
+        -- {
+        --     name = 'Rectangle',
+        --     transform = {position = {x = 400, y = 300}},
+        --     components = {
+        --         {
+        --             script = 'rope.builtins.colliders.rectangle_collider',
+        --             arguments = {
+        --                 width = 300,
+        --                 height = 200
+        --             },
+        --         },
+        --     }
+        -- },
+        -- {
+        --     name = 'Circle',
+        --     components = {
+        --         {
+        --             script = 'rope.builtins.colliders.circle_collider',
+        --             arguments = {radius = 100},
+        --         },
+        --         {script = 'rope.builtins.mouse.link'}
+        --     }
+        -- },
     }
 }
